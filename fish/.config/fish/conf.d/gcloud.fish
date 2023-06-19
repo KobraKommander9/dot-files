6886b837f12978b631bb8e2b8b5d2db0d6c2096d
Ed <ed.kilgore@tcnbroadcasting.com>
2023-06-19 11:44:46 -0600

    Update plugins and gcloud python

--- fish/.config/fish/conf.d/gcloud.fish
+++ fish/.config/fish/conf.d/gcloud.fish
@@ -3,6 +3,8 @@
 # linkerd
 fish_add_path $HOME/.linkerd2/bin/
 
+set -gx CLOUDSDK_PYTHON python
+
 if test -f "$HOME/google-cloud-sdk/path.fish.inc"
-  source "$HOME/google-cloud-sdk/path.fish.inc"
+    source "$HOME/google-cloud-sdk/path.fish.inc"
 end
