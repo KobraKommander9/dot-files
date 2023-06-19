c71ea6f8715a104b9a6026759c3099087218613c
Ed <ed.kilgore@tcnbroadcasting.com>
2023-06-19 16:14:24 -0600

    feat: add lazygit with nvim keymappings; add zellij swap layout to accommodate larger floating panes; update plugins

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/KobraKommander9/KobraVim/blob/main/lua/kobra/core/config/keymaps.lua
--- Add any additional keymaps here
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+
+  -- do not create keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+map("n", "<leader>gg", function()
+  os.execute("zellij run -f -n lazygit --cwd " .. vim.fn.getcwd() .. " -- lazygit")
+end, { desc = "Open LazyGit with Zellij" })
--- nvim/.config/nvim/lua/config/options.lua
+++
@@ -1,3 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/KobraKommander9/KobraVim/blob/main/lua/kobra/core/config/options.lua
--- Add any additional options here
