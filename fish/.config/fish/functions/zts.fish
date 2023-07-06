06b65ef9990cd93a22f6a486e381f44146981655
Ed <ed.kilgore@tcnbroadcasting.com>
2023-07-06 10:06:00 -0600

    feat: add zellij options

--- fish/.config/fish/functions/zts.fish
+++ fish/.config/fish/functions/zts.fish
@@ -2,14 +2,10 @@
 
 function zts -d "list zellij sessions"
     set zj_sessions (zellij list-sessions)
-    if test -z $zj_sessions
-        return 0
-    end
-
     set selected (printf "%s\n" $zj_sessions | fzf --prompt="Zellij Session  " --height=~50% --layout=reverse --border --exit-0 --bind "ctrl-n:down,ctrl-e:up")
-    if test -z $selected
+    if test -z "$selected"
         return 0
     else
-        zellij attach $selected
+        zellij attach "$selected"
     end
 end
