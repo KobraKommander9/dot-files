c8d02fe532a4dfc3b732e3dbf97444db38e4f6c4
Ed <ed.kilgore@tcnbroadcasting.com>
2024-02-02 14:29:26 -0700

    feat(fish): add t/timeout options to gocover

--- fish/.config/fish/functions/gocover.fish
+++ fish/.config/fish/functions/gocover.fish
@@ -1,15 +1,16 @@
 #!/usr/bin/env fish
 
 function gocover
-    argparse h/help c/clean o/open -- $argv
+    argparse h/help c/clean 't/timeout=?' o/open -- $argv
     or return
 
     if set -ql _flag_help
-        echo "gocover [-h|--help] [-o|--open] [path]"
-        echo "  -h, --help  show this help"
-        echo "  -c, --clean remove all generated test files"
-        echo "  -o, --open  open the coverage report in a browser"
-        echo "  path        the path to the package to test; defaults to ./..."
+        echo "gocover [-h|--help] [-c|--clean] [-t|--timeout] [-o|--open] [path]"
+        echo "  -h, --help    show this help"
+        echo "  -c, --clean   remove all generated test files"
+        echo "  -t, --timeout set the timeout for the tests"
+        echo "  -o, --open    open the coverage report in a browser"
+        echo "  path          the path to the package to test; defaults to ./..."
         return 0
     end
 
@@ -41,7 +42,12 @@ function gocover
 
     mkdir -p cover
 
-    go test -cover -covermode count -coverprofile cover/gocover.tmp $dir
+    if test -n "$_flag_timeout"
+        go test -cover -covermode count -coverprofile cover/gocover.tmp -timeout $_flag_timeout $dir
+    else
+        go test -cover -covermode count -coverprofile cover/gocover.tmp $dir
+    end
+
     cat cover/gocover.tmp | grep -v $ignore >cover/gocover.out && rm cover/gocover.tmp
     go tool cover -func cover/gocover.out -o cover/gocover.func
     cat cover/gocover.func | tail -1 | sed 's/[[:space:]][[:space:]]*/ /g'
