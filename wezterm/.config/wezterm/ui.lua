f3ba933a0d5f6e4cd3457e16da3a134bf2dc80b9
Ed <ed.kilgore@tcnbroadcasting.com>
2024-03-15 12:22:34 -0600

    feat(wezterm): put tab bar on bottom

--- wezterm/.config/wezterm/ui.lua
+++ wezterm/.config/wezterm/ui.lua
@@ -22,7 +22,7 @@ end
 
 function M.apply(config)
   config.enable_tab_bar = true
-  config.tab_bar_at_bottom = false
+  config.tab_bar_at_bottom = true
 
   config.window_background_opacity = 0.9
   config.initial_rows = 50
