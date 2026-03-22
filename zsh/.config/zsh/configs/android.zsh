03075229cb9b4a5451e7ce61d58bdbdc0eb821b8
Ed Kilgore <drakonix.dev@gmail.com>
2026-03-22 10:58:42 -0400

    update android config

--- zsh/.config/zsh/configs/android.zsh
+++ zsh/.config/zsh/configs/android.zsh
@@ -1,12 +1,13 @@
-export ANDROID_HOME=$HOME/Android/Sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-
 case "$(uname)" in
   Darwin)
+    export ANDROID_HOME=$HOME/Library/Android/sdk
     export QT_QPA_PLATFORM=cocoa
     ;;
   *)
+    export ANDROID_HOME=$HOME/Android/Sdk
     export QT_QPA_PLATFORM=wayland
     ;;
 esac
+
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
