e1378696928cb67fd47741ff936848a903bd1637
Ed <ed.kilgore@tcnbroadcasting.com>
2026-03-26 10:16:16 -0400

    dev plugins

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -1,22 +1,23 @@
 return {
   {
     "KobraKommander9/KobraVim",
-    -- dev = true,
+    dev = true,
     version = false,
   },
-  -- {
-  --   "KobraKommander9/vintage-rose.nvim",
-  --   dev = true,
-  --   opts = {
-  --     cache = false,
-  --     lush = {
-  --       enabled = true,
-  --     },
-  --   },
-  -- },
+  {
+    "KobraKommander9/vintage-rose.nvim",
+    dev = true,
+    opts = {
+      cache = false,
+      lush = {
+        enabled = true,
+      },
+    },
+  },
   {
     "KobraKommander9/bookwyrm.nvim",
     branch = "setup",
+    dev = true,
   },
   {
     "rktjmp/lush.nvim",
