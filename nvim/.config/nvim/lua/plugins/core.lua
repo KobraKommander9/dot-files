ee9d343ab0022d536e0a4bf24fc77994b967dc77
drakonix <drakonix.dev@gmail.com>
2026-07-17 16:28:33 -0400

    update

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -1,25 +1,30 @@
 return {
-  {
-    "KobraKommander9/KobraVim",
-    -- dev = true,
-    version = false,
-  },
   -- {
-  --   "KobraKommander9/vintage-rose.nvim",
+  --   "KobraKommander9/KobraVim",
   --   dev = true,
-  --   opts = {
-  --     cache = false,
-  --     lush = {
-  --       enabled = true,
-  --     },
-  --   },
+  --   version = false,
   -- },
   {
-    "KobraKommander9/bookwyrm.nvim",
-    branch = "setup",
+    "KobraKommander9/vintage-rose.nvim",
+    -- dev = true,
+    -- opts = {
+    -- cache = false,
+    -- lush = {
+    --   enabled = true,
+    -- },
+    config = true,
   },
   {
-    "rktjmp/lush.nvim",
-    cmd = { "Lushify" },
+    "KobraKommander9/bookwyrm.nvim",
+    -- branch = "setup",
+    -- dev = true,
+  },
+  {
+    "folke/lazydev.nvim",
+    opts = {
+      library = {
+        { path = "bookwyrm", words = { "bookwyrm" } },
+      },
+    },
   },
 }
